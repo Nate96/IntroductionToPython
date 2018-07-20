555f9bb1ccb8676bc8166ccfcbbdc669b6ab1289
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-20 09:27:10 -0500

    Transportation
    transfered the project to the right location

---
+++ Exercise_One.py
@@ -0,0 +1,30 @@
+# This is the first programing exercise
+# Goal: To get familiar with python
+if __name__ == "__main__":
+    # declare two variables. Name them x and y.
+    # set x to equal 5 and y to equal 6.
+    x = 5
+    y = 6
+
+    # print out hello world with the print() method
+    print("hello world")
+
+    # add x and y together and set them equal to z.
+    z = x + y
+    print(z)
+
+    # subtract x and y and set it equal to z.
+    z = x - y
+    # print out z
+    print(z)
+
+    # multiply x and y together.
+    z = x * y
+    # print out z
+    print(z)
+
+    # divide x and y
+    z = x/y
+
+    # print out z
+    print(z)
