ae8311fae7616ce85442e29c2c08204cae356a2e
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-20 09:55:06 -0500

    Took out lines of code for the kids to fill in and added step numbers

--- Exercise_One.py
+++ Exercise_One.py
@@ -1,30 +1,33 @@
 # This is the first programing exercise
 # Goal: To get familiar with python
+# Things to use: +, -, /, *, and print()
 if __name__ == "__main__":
+    # 1.)
     # declare two variables. Name them x and y.
     # set x to equal 5 and y to equal 6.
-    x = 5
-    y = 6
 
-    # print out hello world with the print() method
-    print("hello world")
+    # 2.)
+    # print out "hello world" with the print() method
 
+    # 3.)
     # add x and y together and set them equal to z.
-    z = x + y
+    z =
     print(z)
 
+    # 4.)
     # subtract x and y and set it equal to z.
-    z = x - y
+    z =
     # print out z
-    print(z)
 
+    # 5.)
     # multiply x and y together.
-    z = x * y
+    z =
+    # 6.)
     # print out z
-    print(z)
 
+    # 7.)
     # divide x and y
-    z = x/y
+    z =
 
+    # 8.)
     # print out z
-    print(z)
